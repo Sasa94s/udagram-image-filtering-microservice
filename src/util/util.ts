ce2487c91294e6ee416c88d9681c119741c0c199
Mostafa <abdelaleem@gmx.com>
2021-11-30 02:06:08 +0200

    feat: add filteredimage API

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,25 +1,24 @@
-import fs from "fs";
-import Jimp = require("jimp");
+import fs from 'fs';
+import Jimp from 'jimp';
 
-// filterImageFromURL
-// helper function to download, filter, and save the filtered image locally
-// returns the absolute path to the local image
-// INPUTS
-//    inputURL: string - a publicly accessible url to an image file
-// RETURNS
-//    an absolute path to a filtered image locally saved file
+/**
+ * filterImageFromURL
+ * helper function to download, filter, and save the filtered image locally
+ * @param inputURL string - a publicly accessible url to an image file
+ * @returns Promise<string> an absolute path to a filtered image locally saved file
+ */
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
       const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      const outPath =
+        `/tmp/filtered.${Math.floor(Math.random() * 2000)}.jpg`;
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
-          resolve(__dirname + outpath);
+        .write(__dirname + outPath, () => {
+          resolve(__dirname + outPath);
         });
     } catch (error) {
       reject(error);
@@ -27,11 +26,11 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
   });
 }
 
-// deleteLocalFiles
-// helper function to delete files on the local disk
-// useful to cleanup after tasks
-// INPUTS
-//    files: Array<string> an array of absolute paths to files
+/**
+ * deleteLocalFiles
+ * helper function to delete files on the local disk useful to cleanup after tasks
+ * @param files Array<string> an array of absolute paths to files
+ */
 export async function deleteLocalFiles(files: Array<string>) {
   for (let file of files) {
     fs.unlinkSync(file);
